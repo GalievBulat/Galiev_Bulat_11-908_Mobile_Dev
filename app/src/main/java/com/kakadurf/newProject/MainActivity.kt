75c6d22d36fe34818bbab0e2c004e6f037b3eb8c
Your Name <kakadurf@gmail.com>
2020-09-21 07:08:35 +0300

    done

--- app/src/main/java/com/kakadurf/newProject/MainActivity.kt
+++ app/src/main/java/com/kakadurf/newProject/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.kakadurf.newProject
 
+import android.app.ActionBar
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getSupportActionBar()?.hide();
+        change_the_pr.setOnClickListener{
+            et_setName.visibility= View.VISIBLE
+            main_name.visibility = View.INVISIBLE
+            button_ok.visibility= View.VISIBLE
+        }
+        button_ok.setOnClickListener{
+            et_setName.visibility= View.INVISIBLE
+            main_name.text = et_setName.text
+            main_name.visibility = View.VISIBLE
+            button_ok.visibility= View.INVISIBLE
+        }
     }
 }
