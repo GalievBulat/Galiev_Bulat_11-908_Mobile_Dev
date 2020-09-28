a854e9a60f63f2fd7d404645b7b6c3bacaaa4ffe
Your Name <kakadurf@gmail.com>
2020-09-28 13:23:21 +0300

    registration_done

--- app/src/main/java/com/kakadurf/newProject/MainActivity.kt
+++ app/src/main/java/com/kakadurf/newProject/MainActivity.kt
@@ -1,27 +1,45 @@
 package com.kakadurf.newProject
 
-import android.app.ActionBar
-import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getSupportActionBar()?.hide();
-        change_the_pr.setOnClickListener{
-            et_setName.visibility= View.VISIBLE
-            main_name.visibility = View.INVISIBLE
-            button_ok.visibility= View.VISIBLE
+        supportActionBar?.hide()
+        intent.extras?.let {
+            tv_main_name.text =  it.getCharSequence("name")
+            tv_user_id.text = "@" + it.getCharSequence("email")
+            tv_nick.text =  it.getCharSequence("name")
+            tv_annot.text = "@" + it.getCharSequence("email")
+            val dateFormat = SimpleDateFormat("yyyy", Locale.getDefault())
+            val dateText = dateFormat.format(Calendar.getInstance().time)
+            if (it.getCharSequence("age").toString().matches(Regex("\\d+"))) {
+                val time = dateText.toInt() - it.getCharSequence("age").toString().toInt()
+                tv_reg.text = "Регистрация: " + (time) + "г."
+            }
         }
-        button_ok.setOnClickListener{
+        bt_change_the_pr.setOnClickListener{
+            et_setName.visibility= View.VISIBLE
+            tv_main_name.visibility = View.INVISIBLE
+            bt_ok.visibility= View.VISIBLE
+        }
+        bt_ok.setOnClickListener{
             et_setName.visibility= View.INVISIBLE
-            main_name.text = et_setName.text
-            main_name.visibility = View.VISIBLE
-            button_ok.visibility= View.INVISIBLE
+            tv_main_name.text = et_setName.text
+            tv_main_name.visibility = View.VISIBLE
+            bt_ok.visibility= View.INVISIBLE
+        }
+        iv_back2.setOnClickListener{
+            onBackPressed()
         }
     }
+
 }
---
+++ app/src/main/java/com/kakadurf/newProject/RegistrationActivity.kt
@@ -0,0 +1,24 @@
+package com.kakadurf.newProject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_registation.*
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registation)
+        bt_registrate.setOnClickListener{
+            val name = et_name.text
+            val email = et_email.text
+            val age = et_age.text
+            val intent = Intent(this,MainActivity::class.java)
+            intent.putExtra("name",name)
+            intent.putExtra("email",email)
+            intent.putExtra("age",age)
+            println(name)
+            startActivity(intent)
+        }
+    }
+}
